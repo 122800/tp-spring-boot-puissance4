19b41ddfc6b55befb6f803a15b264268f0d169c7
Leovi Nutakor <lvnutakor@gmail.com>
2017-12-14 14:07:15 +0100

    First commit

---
+++ src/main/java/com/igs/ipi/tpspringbootnutakor/TpSpringbootNutakorApplication.java
@@ -0,0 +1,12 @@
+package com.igs.ipi.tpspringbootnutakor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringbootNutakorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringbootNutakorApplication.class, args);
+	}
+}
